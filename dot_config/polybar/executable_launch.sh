78927d320a500d27fbcf6800ef63b5ea0fee73da
Chris Hosegood <chris.hosegood@gmail.com>
2023-07-08 09:38:38 +0100

    alacritty trans and rmeove network device from polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -4,7 +4,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
 for monitor in $(xrandr --query | grep "\bconnected" | cut -d " " -f1); do
   echo "Starting on monitor '$monitor'"
-  MONITOR="$monitor" polybar primary-i3 &
+  MONITOR="$monitor" polybar main &
 done
 
 echo "Polybars launched"
